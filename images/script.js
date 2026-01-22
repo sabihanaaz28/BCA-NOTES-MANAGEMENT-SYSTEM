d8ff2c7c89445e86f4523dda59f6870f9bb2a39f
Sabiha Naaz <sabihanaaz182@gmail.com>
2026-01-22 22:38:48 +0530

    Delete images/script.js

--- images/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('active');
-});
-
-
-
